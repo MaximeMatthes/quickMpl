31e5e6ace7012811eda61be2fbf176a6ceb378dc
MaximeMatthes <maximematthes@gmail.com>
2020-09-28 16:40:50 +0200

    added subplots_arrangement class

--- quickMpl.py
+++ quickMpl.py
@@ -5,19 +5,19 @@ import numpy as np
 
 class imshow_multiple():
     def __init__(self,figsize = (7,7),cmap = 'hot', **kwargs):
-    	'''
-    	Initialize the imshow multiple class which allows to display quantitative figures while keeping the same scale between them.
-    	Using this method you can conveniently add more images to show and the class will automatically update the color scale of the whole set of images.
-    	
-		Parameters
-		----------
-    	figsize = (7,7)
+        '''
+        Initialize the imshow multiple class which allows to display quantitative figures while keeping the same scale between them.
+        Using this method you can conveniently add more images to show and the class will automatically update the color scale of the whole set of images.
+        
+        Parameters
+        ----------
+        figsize = (7,7)
 
-    	cmap = 'hot'
+        cmap = 'hot'
 
-    	 **kwargs:
-    	 	img_stack: a stack of images to display
-    	'''
+         **kwargs:
+            img_stack: a stack of images to display
+        '''
         self.figsize = figsize
         self.axes = []
         self.figures = []
@@ -30,13 +30,13 @@ class imshow_multiple():
                 self.add_new(image)
                 
     def add_new(self,image):
-    	'''
-    	Add a new image to the class, plot it and updates the other plots and colorbars
-		
-		Parameters
-		----------
-		image: a new image to plot
-		'''
+        '''
+        Add a new image to the class, plot it and updates the other plots and colorbars
+        
+        Parameters
+        ----------
+        image: a new image to plot
+        '''
 
         fig,ax = plt.subplots()
         self.figures.append(fig)
@@ -48,12 +48,12 @@ class imshow_multiple():
         self.harmonize()
             
     def replot(self,image):
-    	'''
-		Takes the given image and plot it into a new figure, adapts the scale to previous images and replot them if the new image goes beyond.
-		Parameters
-		----------
-		image: a new image to plot
-    	'''
+        '''
+        Takes the given image and plot it into a new figure, adapts the scale to previous images and replot them if the new image goes beyond.
+        Parameters
+        ----------
+        image: a new image to plot
+        '''
 
 
         replot_flag = False
@@ -80,9 +80,9 @@ class imshow_multiple():
             self.cbar[-1] = self.figures[-1].colorbar(self.cax[-1])
 
     def harmonize(self):
-    	'''
-		Harmonizes the colorbars so that they actually correspond to the graphs.
-    	'''
+        '''
+        Harmonizes the colorbars so that they actually correspond to the graphs.
+        '''
 
         for j,cbar in enumerate(self.cbar):
             self.cbar[j].remove()
@@ -91,24 +91,90 @@ class imshow_multiple():
 
 
     def set_titles(self, title_list):
-    	'''
-    	Plot the titles associated to each image
+        '''
+        Plot the titles associated to each image
 
-		Parameters
-		----------
-		title_list: list of titles to use for the images (has to be ordered)
-		'''
+        Parameters
+        ----------
+        title_list: list of titles to use for the images (has to be ordered)
+        '''
 
         for j,title in enumerate(title_list):
             self.axes[j].set_title(title)
 
     def save_figs(self, name_list):
-    	'''
-    	saves the images into file of name inside the name_list
+        '''
+        saves the images into file of name inside the name_list
 
-		Parameters
-		----------
-		name_list: list of save_files to use as location for the images (has to be ordered)
-		'''
+        Parameters
+        ----------
+        name_list: list of save_files to use as location for the images (has to be ordered)
+        '''
         for j,save_name in enumerate(name_list):
             self.figures[j].savefig(save_name)
+
+
+
+
+
+
+class subplots_arrangement():
+    def __init__(self, figsize = (7,7), ptype = 'plot', **kwargs):
+        '''
+        subplots arrangement: a class to automatically generate subplots in the best arrangement possible
+        while keeping the quantitative information preserved by displaying the information in the same scales.
+        
+                
+        Parameters
+        ----------
+        figsize = (7,7)
+        
+        ptype = 'plot': choose between plot and imshow to display graphs or arrays/images
+        '''
+        self.figsize = figsize
+        self.axes = []
+        self.images = []
+        self.cax = []
+        self.cbar = []
+        self.type = ptype
+        if 'cmap' in kwargs:
+            self.cmap = kwargs['cmap']
+        else:
+            self.cmap = 'hot'
+        # if ptype == 'plots':
+        #     function = 
+
+    def plot_all(self, stack_elements, xs = None):
+        '''
+        Takes a stack of elements to plot or imshow and generate the best subplot pattern and display
+        the elements while keeping the scale between them
+        
+        Parameters
+        ----------
+        stack_elements: list or np array (along dim 0) of elements to display
+        
+        xs: list or np array (along dim 0) of the x coordinate to use when using the plot mode
+        '''
+        
+        num_elements = len(stack_elements)
+        grid_y = np.int(np.round(np.sqrt(num_elements)))
+        if xs:
+            assert len(xs) == len(stack_elements)
+        if grid_y**2 < num_elements:
+
+            grid_x = np.int(np.ceil(num_elements/grid_y))
+        else:
+            grid_x = grid_y
+        self.fig, self.ax = plt.subplots(grid_y,grid_x, figsize = self.figsize)
+        self.ax = self.ax.ravel()
+        max_val = np.max([np.max(element) for element in stack_elements]) #np.max(stack_elements)
+        min_val = np.min([np.min(element) for element in stack_elements])
+
+        for i,element in enumerate(stack_elements):
+            if self.type == 'plot':
+                if not(xs):
+                    xs = [np.arange(len(element)) for element in stack_elements]
+                self.ax[i].plot(xs[i],element)
+                self.ax[i].set_ylim(min_val,max_val) 
+            elif self.type == 'imshow':
+                self.ax[i].imshow(element,vmin = min_val,vmax = max_val,cmap = self.cmap)
